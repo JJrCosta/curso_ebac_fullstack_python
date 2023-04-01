4f94fc2f9f2240936c63444b44cdfccc12fb3744
JJrCosta <jrpercosta@gmail.com>
2023-04-01 18:32:48 -0300

    Add validation script

--- valida_formulario_EBAC/js/main.js
+++ valida_formulario_EBAC/js/main.js
@@ -1,9 +1,31 @@
-const valorA = document.querySelector('.campo-a').value;
-const valorB = document.querySelector('.campo-b').value;
-let verificaCampo = document.querySelectorAll('input');
+const form = document.querySelector('.valida-form');
+const campoA = document.querySelector('.campo-a');
+const campoB = document.querySelector('.campo-b');
 
-document.addEventListener('focusout', (verificaCampo) => {
-    if (verificaCampo) {
-        document.querySelector('input').classList.add('vazio-invalido');
+function validaCampo() {
+    
+    if (campoA.value != '' && campoB.value != '') {
+        document.querySelector('input').style = ''
+        if (campoB.value > campoA.value) {
+            document.querySelector('.msg').classList.remove('error')
+            document.querySelector('.msg').classList.add('success');
+            document.querySelector('.msg').innerHTML = '<p>Sucesso na validação!</p>';
+        } else {
+            document.querySelector('.msg').classList.remove('success')
+            document.querySelector('.msg').classList.add('error');
+            document.querySelector('.msg').innerHTML = '<p>Falha na validação!</p>';
+        }
+    } else {
+        document.querySelector('.msg').classList.remove('success');
+        document.querySelector('.msg').classList.add('error');
+        document.querySelector('.msg').innerHTML = '<p>Preencha os campos vazios.</p>';
+        document.querySelector('input').style.border = '2px solid red';
     }
-}) 
+    campoA.value = '';
+    campoB.value = '';
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    validaCampo()
+})
