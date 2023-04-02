237d79b39aec9686d98d72a147413ebc78286d02
JJrCosta <jrpercosta@gmail.com>
2023-04-02 14:43:27 -0300

    Feat: user min note input by prompt

--- calculadora_medias/script/main.js
+++ calculadora_medias/script/main.js
@@ -1,4 +1,6 @@
+const listaAtividades = [];
 const listaNotas = [];
+const notaMinima = parseFloat(prompt('Informe a nota de corte (0 à 10):'))
 let linhas = ''
 
 document.getElementById('form').addEventListener('submit', function(e) {
@@ -13,29 +15,35 @@ function criaLinha() {
     const nomeAtividade = document.getElementById('nome-atividade');
     const notaAtividade = document.getElementById('nota-atividade');
 
-    listaNotas.push(parseFloat(notaAtividade.value));
+    if (listaAtividades.includes(nomeAtividade.value)) {
+        alert('Atividade já incluída.')
+    } else {
+        listaAtividades.push(nomeAtividade.value);
+        listaNotas.push(parseFloat(notaAtividade.value));
 
-    let linha = '<tr>';
-    linha += `<td>${nomeAtividade.value}</td>`;
-    linha += `<td>${notaAtividade.value}</td>`;
-    linha += `<td><img src="./img/${notaAtividade.value >= 7 ? 'aprovado' : 'reprovado'}.png"></td>`;
-    linha += '</tr>';
+        let linha = '<tr>';
+        linha += `<td>${nomeAtividade.value}</td>`;
+        linha += `<td>${notaAtividade.value}</td>`;
+        linha += `<td><img src="./img/${notaAtividade.value >= notaMinima ? 'aprovado' : 'reprovado'}.png"></td>`;
+        linha += '</tr>';
 
-    linhas = linha;
-
+        linhas += linha;
+    }
+    
     nomeAtividade.value = '';
     notaAtividade.value = '';
 }
 
 function adicionaLinha() {
-    document.getElementById('tabela').innerHTML += linhas;
+    const novaLinha = document.getElementById('tabela');
+    novaLinha.innerHTML = linhas;
 }
 
 function atualizaMedias() {
     const notaMedia = calculaMedia();
 
-    document.getElementById('nota-final').innerHTML = notaMedia
-    document.getElementById('status-final').innerHTML = notaMedia >= 7 ? '<span class="aprovado">Aprovado</span>' : '<span class="reprovado">Reprovado</span>'
+    document.getElementById('nota-final').innerHTML = notaMedia.toFixed(1)
+    document.getElementById('status-final').innerHTML = notaMedia >= notaMinima ? '<span class="aprovado">Aprovado</span>' : '<span class="reprovado">Reprovado</span>'
 }
 
 function calculaMedia() {
