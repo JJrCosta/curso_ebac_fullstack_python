a4eda39820faa0a30070079103a9cc7e9a754829
JJrCosta <jrpercosta@gmail.com>
2023-09-16 06:37:26 -0300

    Initial commit

---
+++ src/exercicio_ts.ts
@@ -0,0 +1,5 @@
+const calculaArea = (base: number, altura: number): number => base * altura;
+console.log(`A área total é de: ${calculaArea(10, 8)}`);
+
+const saucacao = (nome: string): string => `Olá, ${nome}!`;
+console.log(saucacao('Junior'));
